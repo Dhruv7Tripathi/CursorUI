87b51c63252bc2c1b2e6e91329a713c35dbe617e
Dhruv7Tripathi <tripathidhruv366@gmail.com>
2026-08-09 16:45:24 +0530

    chore: update package.json to include @vercel/analytics dependency

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import Provider from "@/components/providers/provider";
-
+import { Analytics } from "@vercel/analytics/next"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -91,6 +91,7 @@ export default function RootLayout({
             {children}
           </ThemeProvider>
         </Provider>
+        <Analytics />
       </body>
     </html>
   );
